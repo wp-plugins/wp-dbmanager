1dabf3af67ee672fcb87d4d397a92d9475f54ec9
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-01-03 13:36:46 +0000

    Adding wp-dbmanager 2.00
    git-svn-id: https://plugins.svn.wordpress.org/wp-dbmanager/trunk@4392 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dbmanager.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: WP-DBManager
+Plugin URI: http://www.lesterchan.net/portfolio/programming.php
+Description: Manages your Wordpress database. Allows you to optimizee, backup, restore, delete backup database and run selected queries.	
+Version: 2.00
+Author: GaMerZ
+Author URI: http://www.lesterchan.net
+*/
+
+
+/*  Copyright 2005  Lester Chan  (email : gamerz84@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+### Function: Poll Menu
+add_action('admin_menu', 'dbmanager_menu');
+function dbmanager_menu() {
+	if (function_exists('add_menu_page')) {
+		add_menu_page('Database', 'Database', 1, 'database-manager.php');
+	}
+}
+?>
